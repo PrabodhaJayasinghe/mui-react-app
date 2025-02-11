cc0a900db5bc790a73f6504fdb1a4c3885d78325
PrabodhaJayasinghe <110682835+PrabodhaJayasinghe@users.noreply.github.com>
2025-02-11 19:12:43 +0530

    format files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import './App.css'
-import Home from './pages/Home'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
-import SearchAppBar from './components/AppBar'; 
-import Tour from './pages/Tour';
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchAppBar from "./components/AppBar";
+import Tour from "./pages/Tour";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <SearchAppBar />
+      <SearchAppBar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/:id' element={<Tour />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Tour />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
